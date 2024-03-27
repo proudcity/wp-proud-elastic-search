20a82d5d89c02f65bfa2690e505bfc5d4b4add11
Curtis McHale <curtis@curtismchale.ca>
2024-03-27 09:54:42 -0700

    adding the number of search results to the results header

--- templates/search-page.php
+++ templates/search-page.php
@@ -5,7 +5,7 @@
     <?php $search_results->print_filters(); ?>
   </div>
   <div class="col-md-9">
-    <h2 class="h3 margin-top-none">Results</h2>
+	<h2 class="h3 margin-top-none"><?php echo absint( $search_results->get_total_found_posts() ); ?> Results Found</h2>
     <?php $search_results->print_list(); ?>
   </div>
 </div>
